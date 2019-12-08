f57b32557c308e70f1fbffedd863a5bf2a589806
tmbu <54630225+tmbu@users.noreply.github.com>
2019-12-08 22:57:15 +0100

    libft save 1

---
+++ ft_strncpy.c
@@ -0,0 +1,19 @@
+char		*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (!dest || !src)
+		return (0);
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
